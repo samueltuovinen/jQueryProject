f15dc78f50ece2300cd465981e78091f0341faa4
samueltuovinen <91271264+samueltuovinen@users.noreply.github.com>
2022-12-04 15:20:38 +0200

    done

--- JS/skripti.js
+++ JS/skripti.js
@@ -14,19 +14,31 @@ $(document).ready(function () {
         event.stopPropagation();
     });
 
-    // Syötetään inputista itemi "ul"
+    // Syötetään input enterillä
     $("input[type='text']").keypress(function(event) {
         if (event.which === 13) {
+            // tarkistaa onko input field tyhjä
+            if ($("input").val().length === 0) {
+                // Jos tyhjä lähetetään alert ja muutetaan väri
+                alert('Text field is empty');
+                $("input").css("border-color", "red");
+
+                // Jos fieldissä sisältöä, syötetään se näytölle
+            } else { 
             var item = $(this).val();
             $(this).val("");
+            $("input").css("border-color", "rgb(109, 138, 188)");
 
             $("ul").append("<li><span><i class='fa-solid fa-dumpster'></i></span>" + item + "</li>")
+            }
         } 
-    });
+    }); 
+
 
     // mahdollisuus piilottaa input fieldi
     $(".fa-eye-slash").click(function () {
         $("input[type='text']").fadeToggle();
+        $("i[class='fa-solid fa-arrow-turn-down iconsubmit'").fadeToggle();
     });
 
 });
